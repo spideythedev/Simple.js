7273cd02977383add4320474d793bb99c0eca27d
SpideY Dev <fahadmalik2712@gmail.com>
2026-06-26 19:16:43 +0530

    Create tokenizer.test.js

---
+++ packages/compiler/test/tokenizer.test.js
@@ -0,0 +1,54 @@
+import { test } from 'node:test'
+import assert from 'node:assert'
+import { tokenize } from '../src/tokenizer.js'
+import { readFileSync } from 'fs'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+test('tokenizes hello.sjs', () => {
+  const source = readFileSync(join(__dirname, 'fixtures', 'hello.sjs'), 'utf8')
+  const tokens = tokenize(source)
+
+  assert.ok(tokens.length > 0)
+
+  assert.equal(tokens[0].type, 'IDENTIFIER')
+  assert.equal(tokens[0].value, 'name')
+
+  assert.equal(tokens[tokens.length - 1].type, 'EOF')
+})
+
+test('tokenizes keywords', () => {
+  const tokens = tokenize('if true { ret empty }')
+
+  assert.equal(tokens[0].type, 'KEYWORD')
+  assert.equal(tokens[0].value, 'if')
+
+  assert.equal(tokens[2].type, 'KEYWORD')
+  assert.equal(tokens[2].value, 'true')
+
+  assert.equal(tokens[8].type, 'KEYWORD')
+  assert.equal(tokens[8].value, 'empty')
+})
+
+test('tokenizes strings', () => {
+  const tokens = tokenize('name = "Siva"')
+
+  assert.equal(tokens[3].type, 'STRING')
+  assert.equal(tokens[3].value, 'Siva')
+})
+
+test('tokenizes numbers', () => {
+  const tokens = tokenize('x = 42')
+
+  assert.equal(tokens[2].type, 'NUMBER')
+  assert.equal(tokens[2].value, 42)
+})
+
+test('tokenizes @ operator', () => {
+  const tokens = tokenize('log@console("hi")')
+
+  const atToken = tokens.find(t => t.type === 'AT')
+  assert.ok(atToken)
+})
