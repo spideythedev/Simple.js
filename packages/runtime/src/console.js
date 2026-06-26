754ac60e350140f38670fa840ee44bb2c7268011
SpideY Dev <fahadmalik2712@gmail.com>
2026-06-26 19:19:42 +0530

    Create console.js

---
+++ packages/runtime/src/console.js
@@ -0,0 +1,11 @@
+export function log(...args) {
+  console.log(...args)
+}
+
+export function warn(...args) {
+  console.warn(...args)
+}
+
+export function error(...args) {
+  console.error(...args)
+}
